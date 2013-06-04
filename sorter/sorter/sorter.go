3eddb96659d21f2f7ed8f83c53cb10551a038fc3
stevenoyj <steven_oyj@qq.com>
2013-06-04 13:55:38 +0800

    -a

--- sorter/sorter/sorter.go
+++ sorter/sorter/sorter.go
@@ -20,7 +20,53 @@ func readValues(infile string)(values []int, err error){
 
     defer file.Close()
 
+    br := bufio.NewReader(file)
 
+    values = make([]int, 0)
+
+    for{
+        line, isPrefix, err1 := br.ReadLine()
+
+        if err1 != nil{
+            if err1 != io.EOF{
+                err = err1
+            }
+            break
+        }
+
+        if isPrefix{
+            fmt.Println("A too long line, seems unexpected.")
+            return
+        }
+
+        str :=  string(line) //转换字符数组为字符串
+
+        value, err1 := strconv.Atoi(str)
+
+        if err1 != nil{
+            err = err1
+            return
+        }
+
+        values = append(values, value)
+    }
+    return
+}
+
+func writeValues(values []int, outfile string) error{
+	file, err := os.Create(outfile)
+	if err != nil{
+		fmt.Println("Failed to create the outfile file ", outfile)
+		return err
+	}
+
+	defer file.Close()
+
+	for _, value := range values{
+		str := strconv.Itoa(value)
+		file.WriteString(str + "\n")
+	}
+	return nil
 }
 
 
@@ -31,5 +77,10 @@ func main(){
         fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
     }
 
+    values, err := readValues(*infile)
+    if err == nil{
+        fmt.Println("Read values:", values)
+    }else{
+        fmt.Println(err)
+    }
 }
-
